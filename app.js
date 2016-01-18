7d22766172f0b17f578bae0a4757eff05c09ce5b
Zach Simonson <zsimonson26@gmail.com>
2016-01-18 13:09:16 -0800

    wired js and html together, added object constructor and object array

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 //create object constructor for products, and Global allClicks counter
-
-//Place all 15 product objects into a constructor
+function Product(productName, filePath) {
+  this.productName = productName;
+  this.filePath = filePath;
+  this.timesClicked = 0;
+}
+//Place all 15 product objects into an array
+var allProducts = [new Product('bag', 'bag.jpg'),
+                  new Product('banana', 'banana.jpg'),
+                  new Product('boots', 'boots.jpg'),
+                  new Product('chair', 'chair.jpg'),
+                  new Product('cthulu', 'cthulhu.jpg'),
+                  new Product('dragon', 'dragon.jpg'),
+                  new Product('pen', 'pen.jpg'),
+                  new Product('scissors', 'scissors.jpg'),
+                  new Product('shark', 'shark.jpg'),
+                  new Product('sweep', 'sweep.jpg'),
+                  new Product('unicorn', 'unicorn.jpg'),
+                  new Product('usb', 'usb.jpg'),
+                  new Product('water-can', 'water-can.jpg'),
+                  new Product('winer-glass', 'wine-glass.jpg')];
 
 //Display 3 Random, Unique Products for customer on page load.
 
