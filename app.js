3be6a135580f4b6f789f578b61d9c85d42efa61c
Zach Simonson <zsimonson26@gmail.com>
2016-01-18 12:53:00 -0800

    very basic comment instruction on JS

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-//script will go here
+//create object constructor for products, and Global allClicks counter
+
+//Place all 15 product objects into a constructor
+
+//Display 3 Random, Unique Products for customer on page load.
+
+//Event Listeners for all three displays
+
+//Event handlers for all three displays. Should increment counter for clicked product, and display three new random, unique products
+
+//if allClicks => 15, button is not hidden.
